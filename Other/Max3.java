b156191a240cf348d4b9f5f252641593ab7f63d6
hojui <siwa.boonpun@gmail.com>
2017-12-16 09:40:39 +0700

    addall

--- Other/Max3.java
+++ Other/Max3.java
@@ -7,32 +7,38 @@ public class Max3 {
 		System.out.print("input student score : ");
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
-		int[] studentScore = new int[n];
-		int[] studentIndex = new int[n];
+		int max1 = 0; int id1 = 0;
+		int max2 = 0; int id2 = 0;
+		int max3 = 0; int id3 = 0;
  		for (int i = 0 ; i < n ; i++) {
  			System.out.print("student "+(i+1)+" ");
  			int score = kb.nextInt();
- 			studentScore[i] = score;
- 			studentIndex[i] = i;
- 		}
- 		
- 		for (int i = 0 ; i < n-1 ; i++) {
- 			for (int j = 0 ; j < n-1 ; j++) {
- 				if (studentScore[j] < studentScore[j+1]) {
- 					int tmp1 = studentScore[j];
- 					studentScore[j] = studentScore[j+1];
- 					studentScore[j+1] = tmp1;
- 					int tmp2 = studentIndex[j];
- 					studentIndex[j] = studentIndex[j+1];
- 					studentIndex[j+1] = tmp2;
- 				}
+ 			if (i == 0) {
+ 				max1 = score;
+ 				id1 = i;
+ 				continue;
+ 			}
+ 			if (score > max1) {
+ 				max3 = max2;
+ 				max2 = max1;
+ 				max1 = score;
+ 				id3 = id2;
+ 				id2 = id1;
+ 				id1 = i;
+ 			} else if (score<max1 && score>max2) {
+ 				max3 = max2;
+ 				max2 = score;
+ 				id3 = id2;
+ 				id2 = i;
+ 			} else if (score<max2 && score>max3) {
+ 				max3 = score;
+ 				id3 = i;
  			}
  		}
  		
- 		//print value
  		System.out.println("student score max of 3 is ");
- 		for (int i = 0 ; i < 3 ; i++) {
- 			System.out.println("student " + (studentIndex[i]+1) + " : " + studentScore[i]);
- 		}
+ 		System.out.println("student "+(id1+1)+" : "+max1);
+ 		System.out.println("student "+(id2+1)+" : "+max2);
+ 		System.out.println("student "+(id3+1)+" : "+max3);
 	}
 }
